cd010474a3f1b6f318e116898ab0c75958b47bac
Thiago Oliveira <thiago@signaes.co>
2016-09-13 20:15:11 -0300

    feat: user creates equipment

--- spec/features/user_cadaster_equipments_spec.rb
+++ spec/features/user_cadaster_equipments_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 feature 'User cadaster equipments' do
   scenario 'succesfully' do
-    build :category, name: 'Britadeiras'
-    build :category, name: 'Furadeiras'
-    build :category, name: 'Andaimes'
+    create :category, name: 'Britadeiras'
+    create :category, name: 'Furadeiras'
+    create :category, name: 'Andaimes'
 
     visit new_equipment_path
 
     select 'Britadeiras', from: 'Categorias'
+    fill_in 'Modelo', with: 'X870'
+    fill_in 'Número de Série', with: '2234'
+    fill_in 'Número de Inventário', with: '10234'
+    fill_in 'Valor do Equipamento', with: '200'
+    click_on 'Cadastrar'
+
+    expect(page).to have_content 'Britadeiras'
+    expect(page).to have_content 'X870'
+    expect(page).to have_content '2234'
+    expect(page).to have_content '10234'
+    expect(page).to have_content '200'
   end
 end
