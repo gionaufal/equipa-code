201d0c583882854a085b6f9b509ff2b9a06ee175
flavio_e_glaucia <glaucia.isidoro@gmail.com>
2016-09-19 22:02:56 -0300

    WIP add contracts to index page

--- spec/features/user_visits_root_page_spec.rb
+++ spec/features/user_visits_root_page_spec.rb
@@ -1,12 +1,43 @@
 require 'rails_helper'
 
 feature 'visitor visits root page' do
-  scenario 'successfully' do
+  scenario 'visits navbar links' do
 
     visit root_path
 
     expect(page).to have_content('Equipa')
-    expect(page).to have_css('nav', 'orçamento')
     expect(page).to have_css('img', 'Rafael')
+
+    click_on 'Emitir Contrato'
+
+    expect(page).to have_content 'Responsável na obra'
+
+    click_on 'Cadastrar Equipamento'
+
+    expect(page).to have_content 'Número de Série'
+
+    click_on 'Cadastrar Clientes'
+
+    expect(page).to have_content 'Endereço de faturamento'
+
+    click_on 'Enviar Orçamento'
+
+    expect(page).to have_content 'Mensagem'
+
+  end
+
+  scenario 'views contracts' do
+
+    create(:contract)
+    create(:contract)
+    create(:contract)
+    create(:contract)
+    create(:contract)
+    create(:contract)
+
+
+    visit root_path
+
+    expect(page).to have_content 'Campus Code'
   end
 end
