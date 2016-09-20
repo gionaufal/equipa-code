066665989e75ae57fc06a8098e4e8010ef7716d9
Giovanni e Thiago <thiago@signaes.co>
2016-09-19 21:55:28 -0300

    fix: fixes rubocop offenses

--- spec/features/user_navigates_through_budgets_spec.rb
+++ spec/features/user_navigates_through_budgets_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 feature 'user navigates through budgets' do
   scenario 'successfully' do
-    budget = Budget.create(name: 'Manuel', phone: '678467479', mail:'eu@mail.com',
-                        body:'Quero alugar uma frigideira que caiba a minha sogra. Por 5 dias. Obrigado.')
+    budget = Budget.create(name: 'Manuel',
+                           phone: '678467479',
+                           mail: 'eu@mail.com',
+                           body: 'Quero alugar uma frigideira que caiba
+                                  a minha sogra. Por 5 dias. Obrigado.')
 
     visit budgets_path
 
